72afa2baaf0a8c3b7fb809773ef1d8b34b164f5f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-23 20:22:05 -0300

    Conversão de Temperatura - Desafio

---
+++ Temperatura/Conversao.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var celsius = 25.0
+    var fah = (celsius*9)/5 + 32
+    var kelvin = (celsius) + 273.15
+    
+    println("A temperatura em Celsius é: $celsius")
+    println("A temperatura em Fahrenheit é: $fah")
+    println("A temperatura em Kelvin é: $kelvin")
+
+}
